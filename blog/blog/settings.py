1a17bab49a48a9e108b3f4aa207b6e76df06e37b
Rasul <yakhyaevrasul>
2020-03-08 19:05:03 +0500

    added post model and registered in admin

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #custom installed apps
+    'users',
+    'posts',
+    #end of the cus ins apps
 ]
 
 MIDDLEWARE = [
